dbdea312f4aa879acf0ee6af16293bcb6fb8d833
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2023-03-07 22:06:39 -0300

    criação da página de top 10 filmes

--- src/services/api/getMovies.ts
+++
@@ -1,7 +0,0 @@
-const getMovies = async () => {
-    const movies = await fetch('https://api.themoviedb.org/3/movie/1?api_key=fc623a5ca96ebc475b73176b0c3d5b4b&language=en-US')
-    console.log(movies)
-    return movies
-}
-
-export default getMovies;
