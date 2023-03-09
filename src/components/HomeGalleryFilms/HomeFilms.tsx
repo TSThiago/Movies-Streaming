440bbbaf7594006ffb148cc3a4e4a0cb3cd6cd33
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-03-08 23:07:34 -0300

    add a função para buscar a duração dos filmes

--- src/components/HomeGalleryFilms/HomeFilms.tsx
+++ src/components/HomeGalleryFilms/HomeFilms.tsx
@@ -4,11 +4,15 @@ import getMoviesPopular from '../../services/api/getMoviesPopular'
 import { Genre, IFilmList } from '../../types/dataListFilms.interface'
 import getMoviesGenres from '../../services/api/getMoviesGenres'
 import clock from "../../assets/clock.png"
+import getDetails from '../../services/api/getDetails'
+import moment from 'moment'
+
 
 const HomeGalleryFilms = () => {
 
     const [response, setResponse] = useState<IFilmList[]>([])
     const [genre, setGenre] = useState<Genre[]>([])
+    const [runtime, setRunTime] = useState<number[]>([])
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,6 +23,20 @@ const HomeGalleryFilms = () => {
         }
         fetchData()
     }, [])
+
+    useEffect(() => {
+        const duration = async () => {
+            const runTimes = await Promise.all(response.map((item) => handleRunTime(item.movieId)))
+            console.log(runTimes)
+            setRunTime(runTimes)
+        }
+        duration()
+    }, [response])
+
+    const handleRunTime = async (id: number) => {
+        const result = await getDetails(id)
+        return result
+    }
 
     const getGenreNames = (tags: number[]) => {
         const names = tags.map((tag) => {
@@ -31,17 +49,21 @@ const HomeGalleryFilms = () => {
     return (
 
         <section className='containerFilms'>
-            {response.map((item: IFilmList) => (
-                <div className='card'>
+            {response.map((item: IFilmList, index: number) => (
+                <div className='card' key={item.movieId}>
                     <div className='subTitle genders'>
                         {getGenreNames(item.tagsGenre).map((name) => (
                             <span className='gender' key={name} >{name}</span>
                         ))}
                     </div>
                     <img className='imageFilm' src={`https://image.tmdb.org/t/p/w500/${item.background}`} alt="imageHome" />
-                    <span className='subTitle duration'><img src={clock} alt="imageDuration" />1h30min</span>
+                    <span className='subTitle duration'>
+                        <img src={clock} alt="imageDuration" />
+                        <span>{moment.utc().startOf('day').add({ minutes: runtime[index] }).format('HH:mm')}mins</span>
+                    </span>
                     <span className='subTitle title'>{item.title}</span>
                 </div>
+
             ))}
         </section>
     )
---
+++ src/services/api/getDetails.ts
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Details } from '../../types/dataListFilms.interface'
+
+
+const getDetails = async (id: number) => {
+    const keyCode = "fc623a5ca96ebc475b73176b0c3d5b4b"
+
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${keyCode}&language=en-US`)
+    const details : Details = await res.json()
+    const result = details.runtime
+    return result
+
+}
+
+export default getDetails
