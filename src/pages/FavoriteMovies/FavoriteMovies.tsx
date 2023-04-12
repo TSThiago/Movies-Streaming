f1fcf9fe408e97541a2b36e3deec74a23596ae03
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2023-04-12 19:58:11 -0300

    ajustes no tamanho dos cards da página de filmes favoritos

--- src/pages/FavoriteMovies/FavoriteMovies.tsx
+++ src/pages/FavoriteMovies/FavoriteMovies.tsx
@@ -3,20 +3,18 @@ import Footer from '../../components/Footer/Footer'
 import { useEffect, useState } from 'react'
 import "./style.scss"
 import getMoviesPopular from '../../services/api/getMoviesPopular'
-import { Genre, iUserMovies, IFilmList, iMovieList, ResultList } from '../../types/dataListFilms.interface'
+import { Genre, iUserMovies, IFilmList, iMovieList } from '../../types/dataListFilms.interface'
 import getMoviesGenres from '../../services/api/getMoviesGenres'
 import clock from "../../assets/clock.png"
 import getDetails from '../../services/api/getDetails'
 import moment from 'moment'
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import getFavoriteMovies from '../../services/api/getFavoriteMovies'
 import getTopMovies from '../../services/api/getTopMovies'
-import { iUser } from '../../types/user.interface'
 import getMovieDetails from '../../services/api/getMovieDetails'
 import adaptMovies from '../../shared/adapters/adaptFavoriteMovies'
 import { useSelector } from 'react-redux'
 import { iState } from '../../types/redux.interface'
-import getSearchMovies from '../../services/api/getSearchMovies'
 
 const FavoriteMovies = () => {
     const userInfos = useSelector((state: iState) => state.user.user)
@@ -77,7 +75,7 @@ const FavoriteMovies = () => {
 
     const getGenreNames = (tags: Genre[]) => {
         const names = tags.map((tag) => {
-            const genreName = genre.find((item) => item.id === tag.id);
+            let genreName = genre.find((item) => item.id === tag.id);
             return genreName ? genreName.name : "";
         });
         return names;
@@ -93,27 +91,22 @@ const FavoriteMovies = () => {
 
                 </div>
                 {userFavoriteMovies.map((item: iMovieList, index: number) => (
-                    <div className='card' key={item.movieId}>
-                        <div className='subTitle genders'>
-                            {getGenreNames(item.tagsGenre).map((name) => (
-                                <span className='gender' key={name} >{name} </span>
-                            ))}
+                    <Link to={`/Movies/${item.movieId}/${getGenreNames(item.tagsGenre).join(',')}/${runtime[index]}/${text}`}>
+                        <div className='card' style={{ backgroundImage: 'url(https://image.tmdb.org/t/p/original/' + item.background + ')', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', width:'400px', height: '560px', borderRadius: '0'}} key={item.movieId}>
+                            <div className=' movieGenders'>
+                                {getGenreNames(item.tagsGenre).map((name) => (
+                                    <span className='gender' key={name} >{name} </span>
+                                ))}
+                            </div>
+                            <span className='subTitle movieDuration'>
+                                <img src={clock} alt="imageDuration" />
+                                <span>{moment.utc().startOf('day').add({ minutes: runtime[index] }).format('HH:mm')}mins</span>
+                            </span>
+                            <span className='subTitle movieTitle'>{item.title}</span>
                         </div>
-                        <div className='containerImageFilm'>
-                            <Link to={`/Movies/${item.movieId}/${getGenreNames(item.tagsGenre).join(',')}/${runtime[index]}/${text}`}><img className='imageFilm' src={`https://image.tmdb.org/t/p/original/${item.background}`}
-                                alt="imageHome" /></Link>
-
-                        </div>
-                        <span className='subTitle duration'>
-
-                            <img src={clock} alt="imageDuration" />
-                            <span>{moment.utc().startOf('day').add({ minutes: runtime[index] }).format('HH:mm')}mins</span>
-                        </span>
-                        <span className='subTitle title'>{item.title}</span>
-                    </div>
-
+                    </Link>
                 ))}
-            </section>
+            </section >
             <Footer></Footer>
         </>
     )
